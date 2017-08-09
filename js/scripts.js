fbcb108b9b49e555034205977b1e1c61b47fc090
luciano oliveira and snow vilay <luciano+snow@>
2017-08-09 11:58:32 -0700

    work on calculator

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,66 @@
-// var add = function(number1, number2) {
-//   return number1 + number2;
-// };
+// Business (or back-end) logic:
+var number1;
+var number2;
 
-// var number1 = parseInt(prompt("Enter a number:"));
-// var number2 = parseInt(prompt("Enter another number:"));
-// var result = add(number1, number2);
+var result;
+
+//console.log(number1,number2);
+
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
+
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
+
+// User interface (or front-end) logic:
+
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    number1 = parseInt($("#add1").val());
+    number2 = parseInt($("#add2").val());
+    event.preventDefault();
+
+    $('#add').click(function(){
+      result = add(number1, number2);
+      console.log(result);
+    });
+
+    $('#subtract').click(function(){
+      result = subtract(number1, number2);
+      console.log(result);
+    });
+
+    $('#multiplication').click(function(){
+      result = multiply(number1, number2);
+      console.log(result);
+    });
+
+  //  var number1 = parseInt($("#add1").val());
+  //  var number2 = parseInt($("#add2").val());
+  //  var result = add(number1, number2);
+   //
+  //  var number1 = parseInt($("#add1").val());
+  //  var number2 = parseInt($("#add2").val());
+  //  var result = subtract(number1, number2);
+   //
+  //  var number1 = parseInt($("#add1").val());
+  //  var number2 = parseInt($("#add2").val());
+  //  var result = divide(number1, number2);
+   //
+  //  var number1 = parseInt($("#add1").val());
+  //  var number2 = parseInt($("#add2").val());
+  //  var result = multiply(number1, number2);
+
+  $("#output").text(result);
+  });
+});
